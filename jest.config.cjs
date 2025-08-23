4e731bbf4794480c824f154deacf6551273a0317
hello-yuki0409 <bobu2784@gmail.com>
2025-08-23 16:15:25 +0900

    test: switch Jest config to CommonJS

---
+++ jest.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  transform: {
+    "^.+\\.(ts|tsx)$": ["ts-jest", { tsconfig: "tsconfig.jest.json" }],
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+};
