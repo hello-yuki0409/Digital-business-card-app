31138ebc96c58da1183f6b0d180f2fa04aeff070
hello-yuki0409 <bobu2784@gmail.com>
2025-08-26 22:53:29 +0900

    feat: routerに登録パス追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import CardDetailPage from "./components/pages/CardDetailPage.tsx";
+import { CardRegisterPage } from "./components/pages/CardRegisterPage.tsx";
 
 export default function App() {
   return (
     <ChakraProvider>
       <Routes>
         <Route path="/cards/:id" element={<CardDetailPage />} />
+        <Route path="/cards/register" element={<CardRegisterPage />} />
         <Route path="*" element={<Navigate to="/cards/sample_id" replace />} />
       </Routes>
     </ChakraProvider>
