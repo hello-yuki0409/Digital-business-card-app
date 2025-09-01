c9e1b56e441713e4ade4ffb930ffa8c75fdd6583
hello-yuki0409 <bobu2784@gmail.com>
2025-09-02 07:18:00 +0900

    fix: バッチ処理完了のはず5

--- batch/index.ts
+++ batch/index.ts
@@ -1,4 +1,6 @@
-import "dotenv/config";
+import dotenv from "dotenv";
+dotenv.config();
+
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.SUPABASE_URL ?? "";
